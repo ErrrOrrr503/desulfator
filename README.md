4906a2446178014b39e2b8bed173ae8154727223
ErrrOrrr503 <egortit-33@yandex.ru>
2025-05-11 17:49:45 +0300

    Added readme and pictures, optimized delays calculations.

--- README.md
+++ README.md
@@ -1,34 +1,36 @@
 desulfator
 ==========
 
-Rust project for the _Arduino Uno_.
+Pulse charger for dead lead-acid batteries. Can help to (maybe) recover a not that very dead battery, and throw a really dead one away without your toad strangling you about 'maybe there was a way to recover it'.
 
-## Build Instructions
+MCU: Atmega328p.
+Also needed: 
+* BIG coil (I used 0.6mH) that can tolerate high currents
+* Nchannel power mosfet
+* High current diod
+* 12V power supply, i.e. ATX PSU
+
+## Scheme
+
+<img src="pictures/scheme.png" width="500" />
+
+## Algo
+
+Like DC-DC up. Charge coil short-circuiting it to PSU, discharge into the battery.
+
+Frequencies: 7813 kHz short pulses + 122 Hz power pulses.
+
+<img src="pictures/signal.jpg" width="500" />
+
+## Build & flash Instructions
 1. Install prerequisites as described in the [`avr-hal` README] (`avr-gcc`, `avr-libc`, `avrdude`, [`ravedude`]).
 
-2. Run `cargo build` to build the firmware.
+2. Run `cargo build --release` to build the firmware.
 
-3. Run `cargo run` to flash the firmware to a connected board.  If `ravedude`
-   fails to detect your board, check its documentation at
-   <https://crates.io/crates/ravedude>.
+3. Edit the `Ravedude.toml` file to configure your board and serial port.
 
-4. `ravedude` will open a console session after flashing where you can interact
-   with the UART console of your board.
+4. Run `cargo run --release` to flash the firmware.
 
-[`avr-hal` README]: https://github.com/Rahix/avr-hal#readme
-[`ravedude`]: https://crates.io/crates/ravedude
+# Final design
 
-## License
-Licensed under either of
-
- - Apache License, Version 2.0
-   ([LICENSE-APACHE](LICENSE-APACHE) or <http://www.apache.org/licenses/LICENSE-2.0>)
- - MIT license
-   ([LICENSE-MIT](LICENSE-MIT) or <http://opensource.org/licenses/MIT>)
-
-at your option.
-
-## Contribution
-Unless you explicitly state otherwise, any contribution intentionally submitted
-for inclusion in the work by you, as defined in the Apache-2.0 license, shall
-be dual licensed as above, without any additional terms or conditions.
+<img src="pictures/design.jpg" width="500" />
--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,8 @@ const PRESCALER: u32 = 64;
 const TIMER_TOP: u32 = 249;
 const MICROS_PER_TIMER_TICK_MUL: u32 = 4;
 const MICROS_PER_TIMER_TICK_DIV: u32 = 1;
-static MILLIS_COUNTER: Mutex<Cell<u32>> = Mutex::new(Cell::new(0));
+//static MILLIS_COUNTER: Mutex<Cell<u32>> = Mutex::new(Cell::new(0));
+static MICROS_1000_COUNTER: Mutex<Cell<u32>> = Mutex::new(Cell::new(0));
 
 fn time_timer_init(tc0: &arduino_hal::pac::TC0) {
     tc0.tccr0a.write(|w| w.wgm0().ctc());
@@ -24,30 +25,28 @@ fn time_timer_init(tc0: &arduino_hal::pac::TC0) {
         _ => panic!(),
     });
     tc0.timsk0.write(|w| w.ocie0a().set_bit());
-
-    // Reset the global millisecond counter
-    avr_device::interrupt::free(|cs| {
-        MILLIS_COUNTER.borrow(cs).set(0);
-    });
 }
 
 #[avr_device::interrupt(atmega328p)]
 fn TIMER0_COMPA() {
     avr_device::interrupt::free(|cs| {
-        let counter_cell = MILLIS_COUNTER.borrow(cs);
-        let counter = counter_cell.get();
-        counter_cell.set(counter + 1);
+        //let counter_cell = MILLIS_COUNTER.borrow(cs);
+        //let counter = counter_cell.get();
+        //counter_cell.set(counter.wrapping_add(1));
+        let micros_1000_cell = MICROS_1000_COUNTER.borrow(cs);
+        micros_1000_cell.update(|val | {val + 1000});
     })
 }
 
-fn millis() -> u32 {
-    avr_device::interrupt::free(|cs| MILLIS_COUNTER.borrow(cs).get())
-}
+//fn millis() -> u32 {
+//    avr_device::interrupt::free(|cs| MILLIS_COUNTER.borrow(cs).get())
+//}
 
 fn micros(tc0: &arduino_hal::pac::TC0) -> u32 {
     avr_device::interrupt::free(|cs| {
         let cur_counter = tc0.tcnt0.read().bits();
-        MILLIS_COUNTER.borrow(cs).get() * 1000 + cur_counter as u32 * MICROS_PER_TIMER_TICK_MUL / MICROS_PER_TIMER_TICK_DIV
+        let cur_micros_1000 = MICROS_1000_COUNTER.borrow(cs).get();
+        cur_micros_1000.wrapping_add(cur_counter as u32 * MICROS_PER_TIMER_TICK_MUL / MICROS_PER_TIMER_TICK_DIV)
     })
 }
 
@@ -60,33 +59,35 @@ fn main() -> ! {
     // Enable interrupts globally
     unsafe { avr_device::interrupt::enable() };
 
-    const PULSE_US: u32 = 35;
-    const PULSE_PERIOD_US: u32 = 125;
-    const POWER_PULSE_US: u32 = 1000;
-    const POWER_PULSE_CHARGE_US: u32 = 1000;
-    const POWER_PULSE_PERIOD_US: u32 = 8000;
+    const PULSE_US: u32 = 32;
+    const PULSE_PERIOD_US: u32 = 128;
+    const POWER_PULSE_US: u32 = 1024;
+    const POWER_PULSE_CHARGE_US: u32 = 1024;
+    const POWER_PULSE_PERIOD_US: u32 = 8192;
 
     const END_POWER_CHARGE_US: u32 = POWER_PULSE_US + POWER_PULSE_CHARGE_US;
     const PULSES_AMOUNT: u32 = (POWER_PULSE_PERIOD_US - END_POWER_CHARGE_US) / PULSE_PERIOD_US;
     const END_PULSES: u32 = END_POWER_CHARGE_US + PULSES_AMOUNT * PULSE_PERIOD_US;
 
-    let mut led_gate = pins.d13.into_output();
+    let mut led = pins.d13.into_output();
+    let mut gate = pins.d12.into_output();
     
-    led_gate.set_low();
+    led.set_low();
+    gate.set_low();
     arduino_hal::delay_ms(3000); // pause to psu to fully enable.
     loop {
         let cur_time = micros(&dp.TC0) % POWER_PULSE_PERIOD_US;
         match cur_time {
-            0..POWER_PULSE_US => led_gate.set_high(), 
-            POWER_PULSE_US..END_POWER_CHARGE_US => led_gate.set_low(),
+            0..POWER_PULSE_US => {led.set_high(); gate.set_high();}, 
+            POWER_PULSE_US..END_POWER_CHARGE_US => {led.set_low(); gate.set_low();},
             END_POWER_CHARGE_US..END_PULSES => {
                 let hi_freq_time = (cur_time - END_POWER_CHARGE_US) % PULSE_PERIOD_US;
                 match hi_freq_time {
-                    0..PULSE_US => led_gate.set_high(), 
-                    _ => led_gate.set_low(),
+                    0..PULSE_US => {led.set_high(); gate.set_high();}, 
+                    _ => {led.set_low(); gate.set_low();},
                 }
             },
-            _ => led_gate.set_low(),
+            _ => {led.set_low(); gate.set_low();},
         }
     }
 }
